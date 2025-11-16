bb76eaf102846b1bd2be04c5ec40dbf6fc521cb8
basmaxxx <tuvelaprende@gmail.com>
2025-11-16 01:52:09 -0300

    Update RedSocial.cpp

--- RedSocial.cpp
+++ RedSocial.cpp
@@ -56,11 +56,13 @@ void RedSocial::registrar_usuario(string alias, int id){
         this->_conocidos_usuarios_mas_popular = this->_conocidos[id];
     }
     else{
-        //ver si cambia el usuario con mas conocidos
-        auto ittt= this->_conocidos.begin(); //primer elemento del set de amigos del que voy a eliminar
+        //ver si cambia el usuario con mas amigos
+        auto ittt= this->_amigos.begin(); 
         int id_mas_grande= ittt->first;//el id del primero
-        for(ittt; ittt!=this->_conocidos.end(); ittt++){
-            if(ittt->second.size() > this->_conocidos.at(id_mas_grande).size()){
+        for(ittt; ittt!=this->_amigos.end(); ittt++){
+            //si el set asociado al id del iterador es mas grande que el mas grande anterior
+            if(ittt->second.size() > this->_amigos.at(id_mas_grande).size()){
+                //actualizamos el mas grande
                 id_mas_grande= ittt->first;
             }
         }
@@ -103,6 +105,50 @@ void RedSocial::eliminar_usuario(int id){
     int cant_amigos= this->_amigos[id].size();
     this->_cantidad_de_amigos= this->_cantidad_de_amigos-cant_amigos; //le resto las amistades
 
+    //actualizar la lista de conocidos luego de borrar(no solo se tiene encuenta que se borra el mismo)
+    auto it_amigos= this->_amigos[id].begin();//puntero al primero de los amigos
+    
+    //por cada amigo del difunto
+    for(it_amigos; it_amigos != this->_amigos[id].end(); it_amigos++){
+        //id del i amigo
+        int amigo_id= this->_usuario_a_id[*it_amigos];
+
+        //puntero al set de conocidos del i amigo
+        auto it_conocidos_de_amigo= this->_conocidos[amigo_id].begin();
+        vector<int> a_borrar={};
+
+        //por cada conocido del i amigo del difunto
+        for(it_conocidos_de_amigo; it_conocidos_de_amigo != this->_conocidos[amigo_id].end(); it_conocidos_de_amigo++){
+            //por cada conocido, ver que tenga al menos algun amigo que sea amigo de el
+            int id_amigo= this->_usuario_a_id[*it_conocidos_de_amigo];
+
+            //puntero al set de amigos del i amigo 
+            auto it_amigos_de_amigo= this->_amigos[id_amigo].begin();
+            int contador=0;
+            //por cada amigo i del difunto
+            for(it_amigos_de_amigo; it_amigos_de_amigo != this->_amigos[id_amigo].end(); it_amigos_de_amigo++){
+                //por cada conocido de amigo, me fijo que tenga algun amigo 
+                int id_amigos_de_amigos= this->_usuario_a_id[*it_amigos_de_amigo];
+                if(*it_amigos_de_amigo != this->_id_a_usuario[id] &&
+                this->_amigos[amigo_id].find(*it_amigos_de_amigo) != this->_amigos[amigo_id].end()){
+                    contador++;
+                    break;
+                }
+            }
+            if(contador==0){
+                int id_conocido= this->_usuario_a_id[*it_conocidos_de_amigo];
+                a_borrar.push_back(id_conocido);
+            }
+            
+        }
+        //ahora borro los conocidos que sobran del i amigo
+        for(int i=0; i<a_borrar.size(); i++){
+            string alias_a_borrar= this->_id_a_usuario[a_borrar[i]];
+            this->_conocidos[amigo_id].erase(alias_a_borrar);
+        }
+
+    }
+
     string a_eliminar= this->_id_a_usuario[id];
     //recorro el mapa de amigos, eliminando si esta en cada set el id este
     auto it= this->_amigos[id].begin(); //primer elemento del set de amigos del que voy a eliminar
@@ -115,8 +161,8 @@ void RedSocial::eliminar_usuario(int id){
     }
     this->_amigos.erase(id); //lo borramos de la lista de amigos 
 
+    
     //recorro todo el map de conocidos, y me borro si encuentra
-
     auto itt= this->_conocidos.begin(); //primer elemento del set de amigos del que voy a eliminar
 
     for(itt; itt!= this->_conocidos.end(); itt++){
@@ -129,17 +175,21 @@ void RedSocial::eliminar_usuario(int id){
     
     this->_usuarios.erase(id); //borro del set de usuarios
 
-    //ver si cambia el usuario con mas conocidos
-    auto ittt= this->_conocidos.begin(); //primer elemento del set de amigos del que voy a eliminar
-    int id_mas_grande= ittt->first;//el id del primero
-    for(ittt; ittt!=this->_conocidos.end(); ittt++){
-        if(ittt->second.size() > this->_conocidos.at(id_mas_grande).size()){
-            id_mas_grande= ittt->first;
-        }
-    }
 
-    //actualizo usuario ams conocido
-    this->_conocidos_usuarios_mas_popular= this->_conocidos.at(id_mas_grande);
+
+    //ver si cambia el usuario con mas amigos
+    auto ittt= this->_amigos.begin(); 
+    int id_mas_grande= ittt->first;//el id del primero
+    for(ittt; ittt!=this->_amigos.end(); ittt++){
+            //si el set asociado al id del iterador es mas grande que el mas grande anterior
+        if(ittt->second.size() > this->_amigos.at(id_mas_grande).size()){
+              //actualizamos el mas grande
+            id_mas_grande= ittt->first;
+            }
+        }
+
+        //actualizo usuario ams conocido
+        this->_conocidos_usuarios_mas_popular= this->_conocidos.at(id_mas_grande);
 
     string alias= this->_id_a_usuario[id];
 
@@ -222,36 +272,62 @@ void RedSocial::amigar_usuarios(int id_A, int id_B){
     auto it_conocidos_A= this->_conocidos[id_A].begin();
     auto it_conocidos_B= this->_conocidos[id_B].begin();
 
+    vector<string> borrarConocidosA;
+    vector<string> borrarConocidosB;
+
     //chequeo que por cada amigo de a, no este en su lista de conocidos
     //por cada conocido que tiene, se fija si esta en la lista de amigos de el mismo,
     //si esta, lo saca
     for(it_conocidos_A; it_conocidos_A != this->_conocidos[id_A].end(); it_conocidos_A++){
-        this->_amigos[id_A].begin();
+        it_amigos_A= this->_amigos[id_A].begin();
         for(it_amigos_A; it_amigos_A != this->_amigos[id_A].end(); it_amigos_A++){
             if(*it_conocidos_A == *it_amigos_A){
-                this->_conocidos[id_A].erase(*it_conocidos_A);
+                //si el conocido esta en la lista de amigos de a, lo pongo en el vector a borrar
+                borrarConocidosA.push_back(*it_conocidos_A);
+                
             }
         }
+    }
+    //borro todos los elementos del vector
+    for(int i=0; i<borrarConocidosA.size(); i++){
+        //borro cada amigo de la lista de conocidos
+        this->_conocidos[id_A].erase(borrarConocidosA[i]);
     }
 
     //chequeo que por cada amigo de b, no este en su lista de conocidos
     for(it_conocidos_B; it_conocidos_B != this->_conocidos[id_B].end(); it_conocidos_B++){
-        this->_amigos[id_B].begin();
+        it_amigos_B= this->_amigos[id_B].begin();
         for(it_amigos_B; it_amigos_B != this->_amigos[id_B].end(); it_amigos_B++){
             if(*it_conocidos_B == *it_amigos_B){
-                this->_conocidos[id_B].erase(*it_conocidos_B);
+                borrarConocidosB.push_back(*it_conocidos_B);
+
             }
         }
     }
+    //borro todos los elementos del vector
+    for(int i=0; i<borrarConocidosB.size(); i++){
+        this->_conocidos[id_B].erase(borrarConocidosB[i]);
+    }
+
+    //borro de los concoidos
+    this->_conocidos[id_A].erase(aliasB);
+    this->_conocidos[id_B].erase(aliasA);
+
 
     //actualizo la lista de mas concocido
-    auto ittt= this->_conocidos.begin(); //primer elemento del set de amigos del que voy a eliminar
+ //ver si cambia el usuario con mas amigos
+    auto ittt= this->_amigos.begin(); 
     int id_mas_grande= ittt->first;//el id del primero
-    for(ittt; ittt!=this->_conocidos.end(); ittt++){
-            if(ittt->second.size() > this->_conocidos.at(id_mas_grande).size()){
-               id_mas_grande= ittt->first;
+    for(ittt; ittt!=this->_amigos.end(); ittt++){
+            //si el set asociado al id del iterador es mas grande que el mas grande anterior
+        if(ittt->second.size() > this->_amigos.at(id_mas_grande).size()){
+               //actualizamos el mas grande
+            id_mas_grande= ittt->first;
             }
         }
+
+        //actualizo usuario ams conocido
+    this->_conocidos_usuarios_mas_popular= this->_conocidos.at(id_mas_grande);
 
 
 
@@ -275,74 +351,149 @@ void RedSocial::desamigar_usuarios(int id_A, int id_B){
     this->_amigos[id_A].erase(aliasB);
     this->_amigos[id_B].erase(aliasA);
 
+    // Eliminar conocidos que ya no tienen camino para A
+    vector<string> conocidos_a_eliminar_de_A;
+    for(const string& conocido : this->_conocidos[id_A]){
+        int id_conocido = this->_usuario_a_id[conocido];
+        bool tiene_camino = false;
+        
+        // Ver si algún amigo de A es amigo del conocido
+        for(const string& amigo_de_A : this->_amigos[id_A]){
+            int id_amigo = this->_usuario_a_id[amigo_de_A];
+            if(this->_amigos[id_amigo].find(conocido) != this->_amigos[id_amigo].end()){
+                tiene_camino = true;
+                break;
+            }
+        }
+        
+        if(!tiene_camino){
+            conocidos_a_eliminar_de_A.push_back(conocido);
+        }
+    }
+
+    for(const string& conocido : conocidos_a_eliminar_de_A){
+        this->_conocidos[id_A].erase(conocido);
+    }
+
+    // Lo mismo para B
+    vector<string> conocidos_a_eliminar_de_B;
+    for(const string& conocido : this->_conocidos[id_B]){
+        int id_conocido = this->_usuario_a_id[conocido];
+        bool tiene_camino = false;
+        
+        for(const string& amigo_de_B : this->_amigos[id_B]){
+            int id_amigo = this->_usuario_a_id[amigo_de_B];
+            if(this->_amigos[id_amigo].find(conocido) != this->_amigos[id_amigo].end()){
+                tiene_camino = true;
+                break;
+            }
+        }
+        
+        if(!tiene_camino){
+            conocidos_a_eliminar_de_B.push_back(conocido);
+        }
+    }
+
+    for(const string& conocido : conocidos_a_eliminar_de_B){
+        this->_conocidos[id_B].erase(conocido);
+    }
+
     
     //saco de las listas de conocidos
     //puntero a conocidos de a
     auto conocidos_a= this->_conocidos[id_A].begin();
 
-    vector<string> conocidos_a_borrar_A;
+        
+    /* veo de sacar a b, de la lista de conocidos de cada amigo de a, salvo que ese amigo tenga un amigo amigo de 
+    el diferente a a
+    */
+    //vector que guarda los id de los amigos de a los que se borrara de la lista de conocidos, b
+    vector<int> conocidos_a_borrar_A;
+    auto it_amigos_de_A= this->_amigos[id_A].begin();
+    //por cada amigo de a
+    for(it_amigos_de_A; it_amigos_de_A != this->_amigos[id_A].end(); it_amigos_de_A++){
+        //paso de alias a id para ver en su mapa de conocidos
+        int id= this->_usuario_a_id[*it_amigos_de_A];
+        int no_break=0;
 
-    //ciclo por cada conocido de a
-    for(conocidos_a; conocidos_a != this->_conocidos[id_A].end(); conocidos_a++){
-        //ciclo que revise por cada amigo de a
-        auto amigos_a= this->_amigos[id_A].begin();
-        int cont=0;
-        for(amigos_a; amigos_a != this->_amigos[id_A].end(); amigos_a++){
-            if(this->_amigos[id_A].find(*conocidos_a) != this->_amigos[id_A].end()){//si lo encuentra al menos una vez...
-                cont++;//suma 1
-                break;//hace break del ciclo interno
-            }
-        }
-        if(cont == 0){//si no encontro a ninguno, lo tenemos que borrar de la lista de conocidos
-            conocidos_a_borrar_A.push_back(*conocidos_a);
-        }
-    }    
+        //veo si algun amigo de id es amigo de b
+        //puntero a los amigos del i amigo de a
+        auto it_amigos_id= this->_amigos[id].begin();
+        for(it_amigos_id; it_amigos_id != this->_amigos[id].end(); it_amigos_id++){
 
-    //ahora borramos a todos del vector conocidos a borrar
+            //paso el alias del amigo del amigo a id
+            int id_amigo_de_amigo= this->_usuario_a_id[*it_amigos_id];
+
+            //si es diferente de a
+            if(*it_amigos_id != aliasA && this->_amigos[id_amigo_de_amigo].find(aliasB) !=
+               this->_amigos[id_amigo_de_amigo].end()){
+                no_break++;
+                break;
+               }
+        }
+        if(no_break!=0) continue;//si al menos un amigo del amigo es amigo de b, no se borra de la lista de conocidos
+        else{
+            conocidos_a_borrar_A.push_back(id);
+        }
+    }
+    //ahora en el vector conocidos a borrar, tenemos todos los id que hay que borrar b
 
     for(int i=0; i<conocidos_a_borrar_A.size(); i++){
-        this->_conocidos[id_A].erase(conocidos_a_borrar_A[i]);//borra cada uno de esos conocidos(erase del set de strings)
+        this->_conocidos[conocidos_a_borrar_A[i]].erase(aliasB);
     }
 
+
+
     //ahora lo mismo con b
-    auto conocidos_b= this->_conocidos[id_B].begin();
+    vector<int> conocidos_a_borrar_B;
+    auto it_amigos_de_B= this->_amigos[id_B].begin();
+    //por cada amigo de a
+    for(it_amigos_de_B; it_amigos_de_B != this->_amigos[id_B].end(); it_amigos_de_B++){
+        //paso de alias a id para ver en su mapa de conocidos
+        int idB= this->_usuario_a_id[*it_amigos_de_B];
+        int no_break=0;
 
-    vector<string> conocidos_a_borrar_B;
+        //veo si algun amigo de id es amigo de b
+        //puntero a los amigos del i amigo de a
+        auto it_amigos_id= this->_amigos[idB].begin();
+        for(it_amigos_id; it_amigos_id != this->_amigos[idB].end(); it_amigos_id++){
 
-    //ciclo por cada conocido de a
-    for(conocidos_b; conocidos_b != this->_conocidos[id_B].end(); conocidos_b++){
-        //ciclo que revise por cada amigo de a
-        auto amigos_b= this->_amigos[id_B].begin();
-        int cont=0;
-        for(amigos_b; amigos_b != this->_amigos[id_B].end(); amigos_b++){
-            if(this->_amigos[id_B].find(*conocidos_b) != this->_amigos[id_B].end()){//si lo encuentra al menos una vez...
-                cont++;//suma 1
-                break;//hace break del ciclo interno
-            }
+            //paso el alias del amigo del amigo a id
+            int id_amigo_de_amigo= this->_usuario_a_id[*it_amigos_id];
+
+            //si es diferente de a
+            if(*it_amigos_id != aliasB && this->_amigos[id_amigo_de_amigo].find(aliasA) !=
+               this->_amigos[id_amigo_de_amigo].end()){
+                no_break++;
+                break;
+               }
         }
-        if(cont == 0){//si no encontro a ninguno, lo tenemos que borrar de la lista de conocidos
-            conocidos_a_borrar_B.push_back(*conocidos_b);
+        if(no_break!=0) continue;//si al menos un amigo del amigo es amigo de b, no se borra de la lista de conocidos
+        else{
+            conocidos_a_borrar_B.push_back(idB);
         }
-    }    
-
-    //ahora borramos a todos del vector conocidos a borrar
+    }
+    //ahora en el vector conocidos a borrar, tenemos todos los id que hay que borrar b
 
     for(int i=0; i<conocidos_a_borrar_B.size(); i++){
-        this->_conocidos[id_B].erase(conocidos_a_borrar_B[i]);//borra cada uno de esos conocidos(erase del set de strings)
+        this->_conocidos[conocidos_a_borrar_B[i]].erase(aliasA);
     }
 
 
     //ver si cambia el usuario con mas conocidos
-    auto ittt= this->_conocidos.begin(); //primer elemento del set de amigos del que voy a eliminar
+ //ver si cambia el usuario con mas amigos
+    auto ittt= this->_amigos.begin(); 
     int id_mas_grande= ittt->first;//el id del primero
-    for(ittt; ittt!=this->_conocidos.end(); ittt++){
-        if(ittt->second.size() > this->_conocidos.at(id_mas_grande).size()){
+    for(ittt; ittt!=this->_amigos.end(); ittt++){
+            //si el set asociado al id del iterador es mas grande que el mas grande anterior
+        if(ittt->second.size() > this->_amigos.at(id_mas_grande).size()){
+                //actualizamos el mas grande
             id_mas_grande= ittt->first;
+            }
         }
-    }
 
-    //actualizo usuario ams conocido
-    this->_conocidos_usuarios_mas_popular= this->_conocidos.at(id_mas_grande);
+        //actualizo usuario ams conocido
+        this->_conocidos_usuarios_mas_popular= this->_conocidos.at(id_mas_grande);
 }
 
 int RedSocial::obtener_id(string alias) const{
